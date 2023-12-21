216dee38d0d311f4314bdebf5374c560a1aab224
kierin <1@z>
2023-12-21 16:25:10 +0800

    add file

--- src/touch.c
+++ src/touch.c
@@ -2,10 +2,10 @@
 // Created by kierin on 2023/12/21.
 //
 
+#include "../inc/touch.h"
 //TODO: implement get_coordinate and direction
 void get_coordinate(int *x,int*y)
 {
 
 }
 
-#include "../inc/touch.h"

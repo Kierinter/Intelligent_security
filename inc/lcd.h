c628775e51d59fbf5d39b5d06d3ade7518679d04
kierin <1@z>
2023-12-21 14:40:13 +0800

    add file

--- inc/lcd.h
+++ inc/lcd.h
@@ -5,4 +5,8 @@
 #ifndef INTELLIGENT_SECURITY_LCD_H
 #define INTELLIGENT_SECURITY_LCD_H
 
+void lcd_init(void);
+void lcd_close(void);
+void lcd_draw_point(int x, int y, int color);
+void lcd_draw_color(int color);
 #endif //INTELLIGENT_SECURITY_LCD_H
